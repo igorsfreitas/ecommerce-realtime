258d25b68b7ccb3f90d9ff2881b1d79cffe65ed8
Igor Freitas <igorfreitas1992@gmail.com>
2019-07-24 03:47:38 -0300

    fix order item schema

--- database/migrations/1563949044336_order_item_schema.js
+++ database/migrations/1563949044336_order_item_schema.js
@@ -7,7 +7,7 @@ class OrderItemSchema extends Schema {
   up () {
     this.create('order_items', (table) => {
       table.increments()
-      table.integer('id').unsigned()
+      table.integer('product_id').unsigned()
       table.integer('quantity').unsigned()
       table.decimal('subtotal', 12, 2)
       table.integer('order_id').unsigned()
