3070416ebd172a6a0b503527ed5edbc5677b3914
Igor Freitas <igorfreitas1992@gmail.com>
2019-08-02 00:34:23 -0300

    Alter hardcoded host database

--- config/database.js
+++ config/database.js
@@ -84,8 +84,8 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.host),
-      port: Env.get('DB_PORT', '5432'),
+      host: Env.get('DB_HOST', 'ec2-54-243-193-59.compute-1.amazonaws.com'),
+      port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
