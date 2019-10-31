3466b089fbc972bc942ed188929edf94aad14479
Igor Freitas <igorfreitas1992@gmail.com>
2019-10-30 21:31:03 -0300

    Added file upload methods

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -22,6 +22,55 @@ const str_random = async (length=40) => {
     return string
 }
 
+/**
+ * Move single file to especified path
+ * @param { FileJar } fileJar - 0
+ * @param { string } path - 0
+ * @return { Object<FileJar>}
+ */
+const manage_single_upload = async (file, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+    const random_name = await str_random(30)
+    let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+    await file.move(path, {
+        name: filename
+    })
+
+    return file
+}
+
+/**
+ * Move multiple files to especified path
+ * @param { FileJar } fileJar - 0
+ * @param { string } path - 0
+ * 
+ */
+const manage_multiple_uploads = async (fileJar, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+    
+    let successes = [], errors = []
+
+    await Promise.all(fileJar.files.map(async file=>{
+        const random_name = await str_random(30)
+        let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+        await file.move(path, {
+            name: filename
+        })
+
+        if(file.moved()){
+            successes.push(file)
+        }else{
+            errors.push(file.error())
+        }
+    }))
+
+    return {successes, errors}
+}
+
 module.exports = {
-    str_random
+    str_random,
+    manage_single_upload,
+    manage_multiple_uploads
 }
