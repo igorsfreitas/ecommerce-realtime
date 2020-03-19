3f7ada903d1f18436a43f2d5a584483ddcfbb9bf
igorsfreitas <igor_s_freitas@hotmail.com>
2020-03-19 15:22:10 -0300

    Exemplo mensagens customizadas para erros de validação

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -9,6 +9,18 @@ class Register {
       password: 'required|confirmed'
     }
   }
+
+  get messages () {
+    return {
+      'name.required': 'O nome é obrigatório',
+      'surname.required': 'O sobrenome é obrigatório',
+      'email.required': 'O email é obrigatório',
+      'email.email': 'O email é inválido',
+      'email.unique': 'O email já existe',
+      'password.required': 'A senha é obrigatória',
+      'password.confirmed': 'As senhas não são iguais'
+    }
+  }
 }
 
 module.exports = Register
